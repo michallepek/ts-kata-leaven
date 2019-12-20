d8a58bd59f3a74140252dcb3204dc67afe002170
Lepek Michal <m.lepek@tme3c.com>
2019-12-20 13:21:31 +0100

    The Fizz Buzz Kata

---
+++ src/fizz_buzz.ts
@@ -0,0 +1,11 @@
+/*
+|--------------------------------------------------------------------------
+| The FizzBuzz Kata
+|--------------------------------------------------------------------------
+|
+| Print the numbers from 1 to 100. But for multiples of three, print "Fizz”
+| instead of the number and for the multiples of five, print "Buzz".
+| For numbers which are multiples of both three and five, print "FizzBuzz".
+|
+|--------------------------------------------------------------------------
+*/
